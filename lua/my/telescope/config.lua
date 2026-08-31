c72808308a9f55d7693292fa30d63449e7bd5bd8
nabehan <y.nabekura@gmail.com>
2026-08-31 22:32:32 +0900

    feat: enable skk.nvim in telescope input window

--- lua/my/telescope/config.lua
+++ lua/my/telescope/config.lua
@@ -2,29 +2,32 @@
 --  telescope set up
 -- ===================================================================
 local vim = vim
-local telescope = require('telescope')
+local telescope = require("telescope")
 -- telescope 設定
-telescope.setup {
+telescope.setup({
   defaults = {
     preview = {
-      treesitter = true,  -- TSハイライトを無効化
+      treesitter = true, -- TSハイライトを無効化
     },
+
     -- ドロップダウン表示
-    layout_strategy = "horizontal",     -- 水平レイアウトにする
+    layout_strategy = "horizontal", -- 水平レイアウトにする
     layout_config = {
       prompt_position = "top",
-      width           = 0.94,         -- 横幅を調整
-      height          = 0.87,         -- 高さを調整
-      preview_cutoff  = 100,          -- プレビューが切り替わる幅
-      horizontal      = {
-        preview_width = 0.62,         -- プレビューの幅を調整
+      width = 0.94, -- 横幅を調整
+      height = 0.87, -- 高さを調整
+      preview_cutoff = 100, -- プレビューが切り替わる幅
+      horizontal = {
+        preview_width = 0.62, -- プレビューの幅を調整
       },
     },
-    sorting_strategy = "ascending",     -- 候補リストを上から表示
+
+    sorting_strategy = "ascending", -- 候補リストを上から表示
     border = true,
-    winblend = 20,                      -- 30 以上だと境界が見えづらくなる場合あり
+    winblend = 20, -- 30 以上だと境界が見えづらくなる場合あり
     borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
     -- borderchars = { "─", "┃", "━", "│", "┌", "┒", "┛", "┕" },
+
     mappings = {
       i = {
         ["<C-h>"] = "which_key",
@@ -34,6 +37,7 @@ telescope.setup {
       },
     },
   },
+
   pickers = {
     -- grep_string = {
     --  additional_args = function()
@@ -42,52 +46,51 @@ telescope.setup {
     --  end
     -- },
   },
+
   extensions = {
     fzf = {
-      fuzzy = true,                         -- true: fuzzy検索を有効にする
-      override_generic_sorter = true,       -- Telescopeのソートを置き換える
+      fuzzy = true, -- true: fuzzy検索を有効にする
+      override_generic_sorter = true, -- Telescopeのソートを置き換える
       override_file_sorter = true,
-      case_mode = "smart_case",             -- smart_case / ignore_case / respect_case
+      case_mode = "smart_case", -- smart_case / ignore_case / respect_case
     },
+
     file_browser = {
-      hidden = true,                -- ← 隠しファイルも表示
-      follow_symlinks = true,       -- ← シンボリックリンクをたどる
+      hidden = true, -- ← 隠しファイルも表示
+      follow_symlinks = true, -- ← シンボリックリンクをたどる
     },
+
     ["ui-select"] = {
-      require("telescope.themes").get_dropdown {}
+      require("telescope.themes").get_dropdown({}),
     },
   },
-}
+})
+
 -- ===================================================================
 -- Telescope extensions の初期化と設定
 -- ===================================================================
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 -- fzf extension をロード
-telescope.load_extension('fzf')
+telescope.load_extension("fzf")
 -- Frecency = frequency（頻度）+ recency（最近）に基づいてファイル検索
-telescope.load_extension('frecency')
+telescope.load_extension("frecency")
 -- clipboard を telescope から呼び出す
-telescope.load_extension('yank_history')
+telescope.load_extension("yank_history")
 -- Undo を telescope から呼び出す
-telescope.load_extension('undo')
+telescope.load_extension("undo")
 -- telescope から File Navigater
-telescope.load_extension('file_browser')
+telescope.load_extension("file_browser")
 -- "live_grep_args"
-telescope.load_extension('live_grep_args')
+telescope.load_extension("live_grep_args")
 -- To get ui-select loaded and working with telescope
 telescope.load_extension("ui-select")
+
 -- -------------------------------------------------------------------
 -- Visual モードの選択文字列を任意レジスタに保存し取得する汎用関数
 -- @param reg string | nil 使用するレジスタ（例: "v", "a", "_" など）
 --                         nil の場合は "v" をデフォルトとして使用
 -- @return string 選択されたテキスト（前後の空白を除去）
 -- -------------------------------------------------------------------
--- local function get_visual_selection(reg)
---   reg = reg or 'v'                     -- デフォルトは "v" レジスタ
---   vim.cmd('normal! "' .. reg .. 'y')   -- 選択範囲をレジスタにコピー
---   return vim.fn.getreg(reg):gsub("^%s+", ""):gsub("%s+$", "")
--- end
-
 -- Neovim 0.10+ 推奨の書き方 2026-06-20 Sat 05:06:02
 local function get_visual_selection()
   -- 現在のモード（v, V, <C-v>）を取得
@@ -105,21 +108,23 @@ end
 -- -------------------------------------------------------------------
 local function escape_for_rg(text)
   -- 前後の空白を除去
-  return text:gsub("^%s+", ""):gsub("%s+$", ""):gsub('([%[%]%(%)%.%+%-%*%?%^%$])', '\\%1')
+  return text:gsub("^%s+", ""):gsub("%s+$", ""):gsub("([%[%]%(%)%.%+%-%*%?%^%$])", "\\%1")
   -- return text:gsub('([%[%]%(%)%.%+%-%*%?%^%$])', '\\%1') -- 前後の空白を残す
 end
+
 -- -------------------------------------------------------------------
 -- 開いているバッファファイルの一覧を取得する
 -- -------------------------------------------------------------------
 local function get_open_buffer_paths()
   local files = {}
   for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
-    if vim.api.nvim_buf_is_loaded(bufnr) and vim.api.nvim_buf_get_name(bufnr) ~= '' then
+    if vim.api.nvim_buf_is_loaded(bufnr) and vim.api.nvim_buf_get_name(bufnr) ~= "" then
       table.insert(files, vim.api.nvim_buf_get_name(bufnr))
     end
   end
   return files
 end
+
 -- -------------------------------------------------------------------
 -- 開いているバッファファイルのみを対象に grep する関数
 -- -------------------------------------------------------------------
@@ -128,10 +133,12 @@ local function grep_open_buffers()
     search_dirs = get_open_buffer_paths(),
     additional_args = function()
       return { "--hidden", "--follow", "--no-ignore" }
-    end
+    end,
   })
 end
+
 vim.api.nvim_create_user_command("GrepOpenBuffers", grep_open_buffers, {})
+
 -- -------------------------------------------------------------------
 -- Visual 選択を使って開いているバッファから grep する関数
 -- -------------------------------------------------------------------
@@ -143,11 +150,12 @@ local function grep_open_buffers_selection()
     default_text = escape_for_rg(search_term),
     additional_args = function()
       return { "--hidden", "--follow", "--no-ignore" }
-    end
+    end,
   })
 end
 
 vim.api.nvim_create_user_command("GrepOpenBuffersSelection", grep_open_buffers_selection, {})
+
 -- -------------------------------------------------------------------
 -- Visual 選択を使って live_grep（通常スキャン）する関数
 -- -------------------------------------------------------------------
@@ -171,24 +179,40 @@ local function live_grep_visual_selection_full()
     default_text = escape_for_rg(search_term),
     additional_args = function()
       return { "--hidden", "--follow", "--no-ignore" }
-    end
+    end,
   })
 end
 
 vim.api.nvim_create_user_command("LiveGrepVisualSelectionFull", live_grep_visual_selection_full, {})
+
 -- -------------------------------------------------------------------
 require("telescope-all-recent").setup({
   -- 必要に応じて設定をカスタマイズ
   default = {
     disable_cwd_weighting = false,
     ignore_patterns = {
-      "*.git/*", "*/tmp/*"
+      "*.git/*",
+      "*/tmp/*",
     },
     -- シンボリックリンクを表示する
     show_symlinks = true,
   },
 })
+
 -- -------------------------------------------------------------------
 vim.api.nvim_create_user_command("TelescopeSelectTab", function()
   require("my.telescope.tabpicker").select_tab()
 end, {})
+
+-- -------------------------------------------------------------------
+-- Telescopu 入力ウィンドウで<C-j>でSkkEnable にする
+-- -------------------------------------------------------------------
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "TelescopePrompt",
+  callback = function(args)
+    vim.keymap.set("i", "<C-j>", "<Cmd>SkkEnable<CR>", {
+      buffer = args.buf,
+      silent = true,
+    })
+  end,
+})
--- lua/my/telescope/keymap.lua
+++ lua/my/telescope/keymap.lua
@@ -2,16 +2,26 @@
 -- telescope keybind
 -- ===================================================================
 local vim = vim
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>f.", "<cmd>Telescope resume<CR>", { desc = "Resume last Telescope" })
 vim.keymap.set("n", "<leader>fp", "<cmd>Telescope yank_history<CR>", { desc = "Paste from yank history" })
 vim.keymap.set("n", "<leader>fn", "<cmd>Telescope file_browser<CR>", { desc = "File Browser" })
 vim.keymap.set("n", "<leader>fu", "<cmd>Telescope undo<CR>", { desc = "View Undo Tree" })
 vim.keymap.set("n", "<leader>fa", "<cmd>Telescope live_grep_args live_grep_args<CR>", { desc = "Live grep with args" })
-vim.keymap.set("n", "<leader>fb", "<cmd>GrepOpenBuffers<CR>" , { desc = "Grep open files only" })
+vim.keymap.set("n", "<leader>fb", "<cmd>GrepOpenBuffers<CR>", { desc = "Grep open files only" })
 vim.keymap.set("v", "<leader>fb", "<cmd>GrepOpenBuffersSelection<CR>", { desc = "Grep selection in open buffers" })
-vim.keymap.set("v", "<leader>fg", "<cmd>LiveGrepVisualSelectionFast<CR>", { desc = "Live grep visual selection (fast)" })
-vim.keymap.set("v", "<leader>fG", "<cmd>LiveGrepVisualSelectionFull<CR>", { desc = "Live grep visual selection (full)" })
+vim.keymap.set(
+  "v",
+  "<leader>fg",
+  "<cmd>LiveGrepVisualSelectionFast<CR>",
+  { desc = "Live grep visual selection (fast)" }
+)
+vim.keymap.set(
+  "v",
+  "<leader>fG",
+  "<cmd>LiveGrepVisualSelectionFull<CR>",
+  { desc = "Live grep visual selection (full)" }
+)
 vim.keymap.set("n", "<leader>ft", "<cmd>TelescopeSelectTab<CR>", { desc = "TAB Select" })
 vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Recent files" })
 vim.keymap.set("n", "<leader>fc", builtin.current_buffer_fuzzy_find, { desc = "Current buffer fuzzy" })
@@ -28,8 +38,15 @@ vim.keymap.set("n", "<leader>fF", function()
     no_ignore = true,
     follow = true,
     find_command = {
-      "fd", "--type", "f", "--hidden", "--follow", "--no-ignore", "--exclude", ".git"
-    }
+      "fd",
+      "--type",
+      "f",
+      "--hidden",
+      "--follow",
+      "--no-ignore",
+      "--exclude",
+      ".git",
+    },
   })
 end, { desc = "Find all files (full scan)" })
 -- -------------------------------------------------------------------
@@ -40,7 +57,7 @@ vim.keymap.set("n", "<leader>fG", function()
   builtin.live_grep({
     additional_args = function()
       return { "--hidden", "--follow", "--no-ignore" }
-    end
+    end,
   })
 end, { desc = "Live grep (full scan)" })
 -- -------------------------------------------------------------------
@@ -51,7 +68,6 @@ vim.keymap.set("n", "<leader>fS", function()
   builtin.grep_string({
     additional_args = function()
       return { "--hidden", "--follow", "--no-ignore" }
-    end
+    end,
   })
 end, { desc = "Grep word under cursor (full scan)" })
-
